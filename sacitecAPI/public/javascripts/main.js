1c7dce6a37d1399d6bcb7a6db3947354245de03e
sabas1080 <s@theinventorhouse.org>
2016-08-27 21:14:27 -0500

    Added tracking line

--- sacitecAPI/public/javascripts/main.js
+++ sacitecAPI/public/javascripts/main.js
@@ -9,9 +9,11 @@ function onScript(){
 		,zoom:16
 	});
 
-	var tiles = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
-
-
+	tiles = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; 2016 OpenStreetMap contributors, Sacitec &copy; 2016 Electronic Cats',
+		key: 'BC9A493B41014CAABB98F0471D759707'
+	});
 
 	mymap.addLayer(tiles);
 
@@ -30,28 +32,31 @@ function onScript(){
 		mymap.addLayer(computer);
 		computer.bindPopup("Usuarios aqui");
 	}
-	
+
 
 	function onlocation(position){
 		//console.log(position);
 		var pos = position.latlng;
 		var computer = L.marker([pos.lat,pos.lng]);
 		mymap.addLayer(computer);
-		//computer.bindPopup("La PC esta aqui").openPopup();
-
+		computer.bindPopup("La PC esta aqui").openPopup();
 		socket.emit('coords:me', {latlng: pos});
 
 	}
 
 	var gps = L.marker([19.210216666666668, -96.17367333333334]);
 	mymap.addLayer(gps);
+	var posizioni = [];
 
 	socket.on('coords:gps', function (data) {
 		console.log(data);
 		var pos = data.latlng;
 		gps.setLatLng([pos.lat,pos.lng]).update();
-		
-		gps.bindPopup( "Hora " + data.hh + ":" + data.mm + ":" + data.ss).openPopup();
+		posizioni.push(data.latlng);
+		//gps.bindPopup( "Hora " + data.hh + ":" + data.mm + ":" + data.ss).openPopup();
+		gps.bindPopup( "CatSat").openPopup();
+		var polyline = L.polyline(posizioni, {color: 'red'}).addTo(mymap);
+		//mymap.fitBounds(polyline.getBounds());
 		//socket.emit('my other event', { my: 'data' });
 	});
 
