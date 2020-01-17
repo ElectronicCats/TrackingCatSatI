e631c939f72113f23c34cc83a686fb981d96dcc9
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2020-01-16 21:54:54 -0600

    render assets

--- app/renderer/components/Header.js
+++ app/renderer/components/Header.js
@@ -1,32 +1,26 @@
 import React, { Component, Fragment } from "react";
-import { Link } from "react-router";
 class Header extends Component {
   render() {
     return (
-      <Fragment>
-        <header className="cd-main-header">
-          <Link to="#0" className="cd-logo">
-            <img src="/assets/img/logo-uci.png" id="log-uci" alt="Logo" />
-          </Link>
-          <Link to="#0" className="cd-logo">
-            <img
-              src="/assets/img/ElectronicCats.png"
-              id="log-elec"
-              alt="Logo"
-            />
-          </Link>
-          <Link to="#0" className="cd-nav-trigger">
-            Menu<span />
-          </Link>
-          <nav className="cd-nav">
-            <ul className="cd-top-nav">
-              <li className="has-children account">
-                <Link to="#0" id="RSSI-data" />
-              </li>
-            </ul>
-          </nav>
-        </header>
-      </Fragment>
+      <header className="cd-main-header">
+        <a href="#0" className="cd-logo">
+          <img src="./assets/img/logo-uci.png" id="log-uci" alt="Logo" />
+        </a>
+        <a href="#0" className="cd-logo">
+          <img src="./assets/img/ElectronicCats.png" id="log-elec" alt="Logo" />
+        </a>
+        <a href="#0" className="cd-nav-trigger">
+          Menu
+          <span />
+        </a>
+        <nav className="cd-nav">
+          <ul className="cd-top-nav">
+            <li className="has-children account">
+              <a href="#0" id="RSSI-data" />
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
