c4e5e36632545ba51d416868c6cb5f2a1b962f00
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2020-02-10 22:30:04 -0600

    delete data test

--- app/renderer/components/Charts/data.js
+++ app/renderer/components/Charts/data.js
@@ -1,211 +1,24 @@
 export const data = [
   {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
+    name: "Grados C",
+    temp: 35,
   },
   {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
+    C: 3000,
+    C: 1398,
+    amt: 2210
   },
   {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
+    C: 2000,
+    C: 9800,
+    amt: 2290
   },
   {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
-  {
-    name: 'Grados Cº', uv: 4000, pv: 2400, amt: 2400,
-  },
-  {
-    name: 'Grados Fº', uv: 3000, pv: 1398, amt: 2210,
-  },
-  {
-    name: 'Grados Yº', uv: 2000, pv: 9800, amt: 2290,
-  },
-  {
-    name: 'Grados Kº', uv: 2780, pv: 3908, amt: 2000,
-  },
-  {
-    name: 'Grados Fº', uv: 1890, pv: 4800, amt: 2181,
-  },
-  {
-    name: 'Grados Kº', uv: 2390, pv: 3800, amt: 2500,
-  },
-  {
-    name: 'Grados Cº', uv: 3490, pv: 4300, amt: 2100,
-  },
+    C: 2780,
+    C: 3908,
+    amt: 2000
+  }
 ];
 
-export const str = "id,29.34,28.3,1025,29.34,1293,3312,14,1422,12334,17,0,0,342,21.9008467,-102.31658777777777,980,10,23432,423432,120.5";
+export const str =
+  "id,29.34,28.3,1025,29.34,1293,3312,14,1422,12334,17,0,0,342,21.9008467,-102.31658777777777,980,10,23432,423432,120.5";
