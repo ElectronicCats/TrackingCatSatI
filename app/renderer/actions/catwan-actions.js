99ccccb2a67462b3905e8022d9e81ebdbb2f457d
Oscar Eduardo Miramontes Raygoza <oscar.eduardo.raygoza@gmail.com>
2020-02-11 23:39:11 -0600

    delate comments

--- app/renderer/actions/catwan-actions.js
+++ app/renderer/actions/catwan-actions.js
@@ -4,28 +4,9 @@ import {
   SEND_COMMAND,
   SENT_COMMAND,
   INCREMENT_EPOCH
-} from "./action-types"; // snag the action type string
+} from "./action-types"; 
 import hash from "object-hash";
 import store from "../store";
-import { Fragment } from "react";
-
-/*  Actions
-    - actions return an object with (minimally) a field called "type," which is a string describing the action to take
-        - the reducer will switch on the action string to modify the state appropriately
-        - we don't use a string directly, we use OBC_SERIAL_RX which is defined in action-types.js to keep things easier to coordinate
-    - we also pass some data into the action (obcdata_in), which the reducer can then pull out and append to the state (or whatever)
-
-    - That's all an action does! Spits out an object with a string called "type", and optionally some data that may be used in changing the state. 
-
-This action returns the following object:
-{
-type: OBC_SERIAL_RX (but turned into a string)
-payload: {
-        receivedStr: obcdata_in (a string from the serial port)
-        // todo: maybe other things in the future?
-    }
-}
-*/
 
 function get_epoch() {
   let state = store.getState();
